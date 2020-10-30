e4e58b657928d918804ed17e27952e189466f2e6
Chams123456 <60320687+Chams123456@users.noreply.github.com>
2020-10-30 19:54:40 +0530

    Add files via upload

---
+++ Arduino_code.ino
@@ -0,0 +1,57 @@
+ //Arduino side code
+//DHT11 Lib
+#include <DHT.h>
+
+//Arduino to NodeMCU Lib
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+
+//Initialise Arduino to NodeMCU (5=Rx & 6=Tx)
+SoftwareSerial nodemcu(5, 6);
+
+//Initialisation of DHT11 Sensor
+#define DHTPIN 4
+DHT dht(DHTPIN, DHT11);
+float temp;
+float hum;
+
+void setup() {
+  Serial.begin(9600);
+
+  dht.begin();
+  nodemcu.begin(9600);
+  delay(1000);
+
+  Serial.println("Program started");
+}
+
+void loop() {
+
+  StaticJsonBuffer<1000> jsonBuffer;
+  JsonObject& data = jsonBuffer.createObject();
+
+  //Obtain Temp and Hum data
+  dht11_func();
+
+
+  //Assign collected data to JSON Object
+  data["humidity"] = hum;
+  data["temperature"] = temp; 
+
+  //Send data to NodeMCU
+  data.printTo(nodemcu);
+  jsonBuffer.clear();
+
+  delay(2000);
+}
+
+void dht11_func() {
+
+  hum = dht.readHumidity();
+  temp = dht.readTemperature();
+  Serial.print("Humidity: ");
+  Serial.println(hum);
+  Serial.print("Temperature: ");
+  Serial.println(temp);
+
+}
---
+++ Nodemcu_code.ino
@@ -0,0 +1,35 @@
+//Include Lib for Arduino to Nodemcu
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+
+//D6 = Rx & D5 = Tx
+SoftwareSerial nodemcu(D6, D5);
+
+
+void setup() {
+  // Initialize Serial port
+  Serial.begin(9600);
+  nodemcu.begin(9600);
+  while (!Serial) continue;
+}
+
+void loop() {
+  
+  StaticJsonBuffer<1000> jsonBuffer;
+  JsonObject& data = jsonBuffer.parseObject(nodemcu);
+
+  if (data == JsonObject::invalid()) {
+    //Serial.println("Invalid Json Object");
+    jsonBuffer.clear();
+    return;
+  }
+
+  Serial.println("JSON Object Recieved");
+  Serial.print("Recieved Humidity:  ");
+  float hum = data["humidity"];
+  Serial.println(hum);
+  Serial.print("Recieved Temperature:  ");
+  float temp = data["temperature"];
+  Serial.println(temp);
+  Serial.println("-----------------------------------------");
+}
